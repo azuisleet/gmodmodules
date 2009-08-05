9ee3309b04777aa43f9bd18f2c9f06d001e8a9f9
AzuiSleet <AzuiSleet@1babe0d6-6514-11de-a3c1-4985b5740c72>
2009-08-05 04:33:22 +0000

    improved gm_slog thanks to ComWalk

--- trunk/gm_slog/gm_slog/gm_slog.cpp
+++ trunk/gm_slog/gm_slog/gm_slog.cpp
@@ -7,52 +7,57 @@
 
 #include "interface.h"
 #include "eiface.h"
+#include "iclient.h"
 
 IVEngineServer	*engine = NULL;
 
 #include <windows.h>
 #include <detours.h>
 
-// void	Cmd_RecvCommand()
-typedef void (__fastcall *Cmd_RecvCommand_t)(void *a,  void *b, void *c);
-void (__fastcall *Cmd_RecvCommand_Orig)(void *a,  void *b, void *c);
-void __fastcall Cmd_RecvCommand_New(void *a,  void *b, void *c);
+ILuaInterface *globalLua;
+
+// virtual bool	ExecuteStringCommand( const char *s ) = 0;
+class CDetour
+{
+public:
+	bool ExecuteStringCommand(const char *s);
+	static bool (CDetour::* ExecuteStringTrampoline)(const char *s);
+
+};
+typedef bool (__thiscall CDetour::* *execstringcmd_t)(const char *);
+
+bool (CDetour::* CDetour::ExecuteStringTrampoline)(const char *s) = 0;
+
+bool CDetour::ExecuteStringCommand(const char *s)
+{
+	IClient *cl = (IClient *)this;
+
+	ILuaObject *func = globalLua->GetGlobal("Cmd_RecvCommand");
+	if(func->isFunction())
+	{
+		globalLua->Push(func);
+
+		globalLua->Push(cl->GetClientName());
+		globalLua->Push(s);
+		globalLua->Push(cl->GetNetworkIDString());
+
+		globalLua->Call(3, 1);
+
+		ILuaObject *returno = globalLua->GetReturn(0);
+		bool ignore = returno->GetBool();
+
+		if(ignore)
+			return true;
+	}
+
+	return (this->*ExecuteStringTrampoline)(s);
+}
 
 #define RECVCMD "\x81\xEC\x08\x05\x00\x00\x56\x8B\xB4\x24\x10\x05\x00\x00"
 #define RECVCMDMASK "xxxxxxxxxxxxxx"
 #define RECVCMDLEN 14
 
 CSigScan RecvCmd_Sig;
-
-struct cdata_t
-{
-	char unknown[20];
-	char name[255];
-};
-
-ILuaInterface *globalLua;
-
-void __fastcall Cmd_RecvCommand_New(void *a, void *b, void *c)
-{
-	const char *cmd;
-	cdata_t *client;
-	_asm mov cmd, edx;
-	_asm mov client, ecx;
-
-	ILuaObject *func = globalLua->GetGlobal("Cmd_RecvCommand");
-	globalLua->Push(func);
-
-	globalLua->Push(client->name);
-	globalLua->Push(cmd);
-
-	globalLua->Call(2, 1);
-
-	ILuaObject *returno = globalLua->GetReturn(0);
-	bool ignore = returno->GetBool();
-
-	if(!ignore)
-		Cmd_RecvCommand_Orig(a, b, c);
-}
 
 LUA_FUNCTION(AppendLog)
 {
@@ -100,12 +105,13 @@ int Start(lua_State *L)
 	bool scan = CSigScan::GetDllMemInfo();
 
 	RecvCmd_Sig.Init((unsigned char *)RECVCMD, RECVCMDMASK, RECVCMDLEN);
-	Cmd_RecvCommand_Orig = (Cmd_RecvCommand_t)RecvCmd_Sig.sig_addr;
+	CDetour::ExecuteStringTrampoline = *(execstringcmd_t)&RecvCmd_Sig.sig_addr;
 
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
 
-	DetourAttach((&(PVOID&)Cmd_RecvCommand_Orig), (&(PVOID&)Cmd_RecvCommand_New) );
+	DetourAttach(&(PVOID&)CDetour::ExecuteStringTrampoline,
+		(PVOID)(&(PVOID&)CDetour::ExecuteStringCommand));
 
 	DetourTransactionCommit();
 
@@ -120,7 +126,8 @@ int Close(lua_State *L)
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
 
-	DetourDetach((&(PVOID&)Cmd_RecvCommand_Orig), (&(PVOID&)Cmd_RecvCommand_New) );
+	DetourDetach(&(PVOID&)CDetour::ExecuteStringTrampoline,
+		(PVOID)(&(PVOID&)CDetour::ExecuteStringCommand));
 
 	DetourTransactionCommit();
 
