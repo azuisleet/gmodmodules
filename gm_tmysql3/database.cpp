6cec1bdbeee3c8a4d283d0ef65008518c413fe69
azuisleet <azuisleet@1babe0d6-6514-11de-a3c1-4985b5740c72>
2010-07-14 02:41:39 +0000

    tmysql3

---
+++ gm_tmysql3/database.cpp
@@ -0,0 +1,170 @@
+#include "gm_tmysql.h"
+
+Database::Database( const char* host, const char* user, const char* pass, const char* db, int port ) :
+	m_strHost(host), m_strUser(user), m_strPass(pass), m_strDB(db), m_iPort(port),
+	m_pThreadPool(NULL)
+{
+}
+
+Database::~Database( void )
+{
+
+}
+
+bool Database::Initialize( CUtlString& error )
+{
+	static my_bool bTrue = true;
+
+	for( int i = NUM_CON_DEFAULT; i; i-- )
+	{
+		MYSQL* mysql = mysql_init(NULL);
+		mysql_options(mysql, MYSQL_OPT_RECONNECT, &bTrue);
+
+		if ( !Connect( mysql, error ) )
+		{
+			return false;	
+		}
+
+		m_vecAvailableConnections.AddToTail( mysql );
+		m_vecAllConnections.AddToTail( mysql );
+	}
+
+	m_pThreadPool = CreateThreadPool();
+
+	ThreadPoolStartParams_t params;
+	params.nThreads = NUM_THREADS_DEFAULT;
+
+	if ( !m_pThreadPool->Start( params ) )
+	{
+		SafeRelease( m_pThreadPool );
+		return false;
+	}
+
+	return true;
+}
+
+bool Database::Connect( MYSQL* mysql, CUtlString& error )
+{
+	if ( !mysql_real_connect( mysql, m_strHost, m_strUser, m_strPass, m_strDB, m_iPort, NULL, 0 ) )
+	{
+		error.Set( mysql_error( mysql ) );
+		return false;
+	}
+
+	return true;
+}
+
+void Database::Shutdown( void )
+{
+	if ( m_pThreadPool != NULL )
+	{
+		m_pThreadPool->Stop();
+		DestroyThreadPool( m_pThreadPool );
+		m_pThreadPool = NULL;
+	}
+
+	FOR_EACH_VEC( m_vecAllConnections, i )
+	{
+		mysql_close( m_vecAllConnections[i] );
+	}
+
+	m_vecAllConnections.Purge();
+}
+
+char* Database::Escape( const char* query )
+{
+	size_t len = Q_strlen( query );
+	char* escaped = new char[len*2+1];
+
+	mysql_escape_string( escaped, query, len );
+
+	return escaped;
+}
+
+bool Database::SetCharacterSet( const char* charset, CUtlString& error )
+{
+	FOR_EACH_VEC( m_vecAllConnections, i )
+	{
+		if ( mysql_set_character_set( m_vecAllConnections[i], charset ) > 0 )
+		{
+			error.Set( mysql_error( m_vecAllConnections[i] ) );
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void Database::QueueQuery( const char* query, int callback, int flags, int callbackref )
+{
+	Query* newquery = new Query( query, callback, flags, callbackref );
+
+	QueueQuery( newquery );
+}
+
+void Database::QueueQuery( Query* query )
+{
+	CJob* job = m_pThreadPool->QueueCall( this, &Database::DoExecute, query );
+}
+
+
+
+void Database::YieldPostCompleted( Query* query )
+{
+	AUTO_LOCK_FM( m_vecCompleted );
+
+	m_vecCompleted.AddToTail( query );
+}
+
+
+void Database::DoExecute( Query* query )
+{
+#ifdef USE_THREAD_LOCAL
+	static THREAD_LOCAL MYSQL* pMYSQL;
+#else
+	CThreadLocalPtr<MYSQL> pMYSQL;
+#endif
+
+	if ( pMYSQL == NULL )
+	{
+		AUTO_LOCK_FM( m_vecAvailableConnections );
+
+		Assert( m_vecAvailableConnections.Size() > 0 );
+
+		pMYSQL = m_vecAvailableConnections.Head();
+		m_vecAvailableConnections.Remove( 0 );
+
+		Assert( pMYSQL );
+	}
+
+	const char* strquery = query->GetQuery();
+	size_t len = query->GetQueryLength();
+
+	int err = mysql_real_query( pMYSQL, strquery, len );
+
+	if ( err > 0 )
+	{
+		mysql_ping( pMYSQL );
+
+		err = mysql_real_query( pMYSQL, strquery, len );
+	}
+
+	if ( err > 0 )
+	{
+		query->SetError( mysql_error( pMYSQL ) );
+		query->SetStatus( QUERY_FAIL );
+		query->SetResult( NULL );
+	}
+	else
+	{
+		query->SetStatus( QUERY_SUCCESS );
+		query->SetResult( mysql_store_result( pMYSQL ) );
+
+		if ( query->GetFlags() & QUERY_FLAG_LASTID )
+		{
+			query->SetLastID( mysql_insert_id( pMYSQL ) );
+		}
+	}
+
+	YieldPostCompleted( query );
+}
