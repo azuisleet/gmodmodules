a886e4fbe44c2b4a6391533ff12ff8850e4ef486
azuisleet <azuisleet@1babe0d6-6514-11de-a3c1-4985b5740c72>
2012-07-21 05:44:25 +0000

    gm_pimpmyride, gm_transmittools: Updated LevelShutdown signature

--- trunk/gm_pimpmyride/gm_pimpmyride/gm_pimpmyride.cpp
+++ trunk/gm_pimpmyride/gm_pimpmyride/gm_pimpmyride.cpp
@@ -9,10 +9,10 @@
 #include "sigscan.h"
 
 // gEntList, from Sourcemod gamedata
-#define LEVELSHUTDOWN "\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xB9\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8"
-#define LEVELSHUTDOWNMASK "x????x????x????x????x????x"
-#define LEVELSHUTDOWNLEN 26
-#define ENTLISTOFFSET 16
+#define LEVELSHUTDOWN "\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xB9\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8"
+#define LEVELSHUTDOWNMASK "x????x????x????x????x"
+#define LEVELSHUTDOWNLEN 21
+#define ENTLISTOFFSET 11
 
 CSigScan LEVELSHUTDOWN_Sig;
 
