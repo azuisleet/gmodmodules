70ec23c27d41fdcd617389033cbf4f5c959d440e
azuisleet <azuisleet@1babe0d6-6514-11de-a3c1-4985b5740c72>
2012-07-21 05:45:52 +0000

    gm_tmysql: static linux build (currently uses a hacked link script)

---
+++ gm_tmysql3_boost/link.sh
@@ -0,0 +1,2 @@
+/usr/bin/g++-4.6  -fPIC    -shared -Wl,-soname,libgm_tmysql.so -o libgm_tmysql.so CMakeFiles/gm_tmysql.dir/database.cpp.o CMakeFiles/gm_tmysql.dir/gm_tmysql.cpp.o /usr/lib/libmysqlclient.a /usr/local/lib/libboost_system.a /usr/local/lib/libboost_thread.a -static-libgcc -static-libstdc++ -lz
+
