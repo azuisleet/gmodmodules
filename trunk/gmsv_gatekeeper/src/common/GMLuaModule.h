60e222bbf5638965d7de8f1bd93238616d4263b6
azuisleet <azuisleet@1babe0d6-6514-11de-a3c1-4985b5740c72>
2012-08-06 02:05:23 +0000

    gm_gatekeeper: Likely functional GM13 build

--- trunk/gmsv_gatekeeper/src/common/GMLuaModule.h
+++ trunk/gmsv_gatekeeper/src/common/GMLuaModule.h
@@ -12,30 +12,40 @@
 
 #include "ILuaInterface.h"
 #include "ILuaObject.h"
+
 #include "ILuaModuleManager.h"
 
-#ifdef WIN32
-	#define GM_EXPORT __declspec(dllexport)
-#else
-	#define GM_EXPORT __attribute__((visibility("default")))
-#endif
-
 // You should place this at the top of your module
+#ifdef GMOD_BETA
 #define GMOD_MODULE( _startfunction_, _closefunction_ ) \
 	ILuaModuleManager* modulemanager = NULL;\
 	int _startfunction_( lua_State* L );\
 	int _closefunction_( lua_State* L );\
-	extern "C" int GM_EXPORT gmod_open( ILuaInterface* i ) \
+	DLL_EXPORT int gmod13_open( ILuaInterface* i ) \
 	{ \
-		if ( !i ) return 0;\
 		modulemanager = i->GetModuleManager();\
 		return _startfunction_( (lua_State*)(i->GetLuaState()) );\
 	}\
-	extern "C" int GM_EXPORT gmod_close( lua_State* L ) \
+	DLL_EXPORT int gmod_close( lua_State* L ) \
 	{\
-		if ( !L ) return 0;\
 		_closefunction_( L );\
 		return 0;\
+	}
+#else
+#define GMOD_MODULE( _startfunction_, _closefunction_ ) \
+	ILuaModuleManager* modulemanager = NULL;\
+	int _startfunction_( lua_State* L );\
+	int _closefunction_( lua_State* L );\
+	DLL_EXPORT int gmod_open( ILuaInterface* i ) \
+	{ \
+		modulemanager = i->GetModuleManager();\
+		return _startfunction_( (lua_State*)(i->GetLuaState()) );\
 	}\
+	DLL_EXPORT int gmod_close( lua_State* L ) \
+	{\
+		_closefunction_( L );\
+		return 0;\
+	}
+#endif
 
-#define LUA_FUNCTION( _function_ ) int _function_( lua_State* L )
+#define LUA_FUNCTION( _function_ ) static int _function_( lua_State* L )
