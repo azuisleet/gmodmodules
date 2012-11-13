8a31eff6e3479e39c32783be61a1c1a384ab0e0b
AzuiSleet@gmail.com <AzuiSleet@gmail.com@1babe0d6-6514-11de-a3c1-4985b5740c72>
2012-11-13 06:14:07 +0000

    gm_tmysql: Refactored to use C++11/ASIO instead of vstdlib

--- trunk/gm_tmysql3/database.h
+++ trunk/gm_tmysql3/database.h
@@ -1,3 +1,9 @@
+#include <deque>
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <asio.hpp>
+
 #define QUERY_SUCCESS true
 #define QUERY_FAIL false
 
@@ -34,10 +40,45 @@ private:
 };
 #endif
 
+// From the boost atomic examples
+template<typename T>
+class waitfree_query_queue {
+public:
+  void push(T * data)
+  {
+    T * stale_head = head_.load(std::memory_order_relaxed);
+    do {
+      data->next = stale_head;
+    } while (!head_.compare_exchange_weak(stale_head, data, std::memory_order_release, std::memory_order_relaxed));
+  }
+  
+  T * pop_all(void)
+  {
+    T * last = pop_all_reverse(), * first = 0;
+    while(last) {
+      T * tmp = last;
+      last = last->next;
+      tmp->next = first;
+      first = tmp;
+    }
+    return first;
+  }
+  
+  waitfree_query_queue() : head_(0) {}
+
+  T * pop_all_reverse(void)
+  {
+    return head_.exchange(0, std::memory_order_consume);
+  }
+private:
+	std::atomic<T *> head_;
+};
+
 class Query
 {
 public:
-	Query( const char* query, int callback = -1, int flags = 0, int callbackref = -1 ) : m_strQuery( query ), m_iCallback( callback ), m_iFlags( flags ), m_iCallbackRef( callbackref ), m_iLastID( 0 )
+	Query( const char* query, int callback = -1, int flags = 0, int callbackref = -1 ) : 
+		m_strQuery( query ), m_iCallback( callback ), m_iFlags( flags ), m_iCallbackRef( callbackref ), m_iLastID( 0 )
 	{
 	}
 
@@ -45,15 +86,15 @@ public:
 	{
 	}
 
-	const char* GetQuery( void ) { return m_strQuery; }
-	int			GetQueryLength( void ) { return m_strQuery.Length(); }
+	const std::string& GetQuery( void ) { return m_strQuery; }
+	size_t		GetQueryLength( void ) { return m_strQuery.length(); }
 
 	int			GetFlags( void ) { return m_iFlags; }
 	int			GetCallback( void ) { return m_iCallback; }
 	int			GetCallbackRef( void ) { return m_iCallbackRef; }
 
-	void		SetError( const char* error ) { m_strError.Set( error ); }
-	const char* GetError( void ) { return m_strError.Get(); }
+	void		SetError( const char* error ) { m_strError.assign( error ); }
+	const std::string& GetError( void ) { return m_strError; }
 
 	void		SetStatus( bool status ) { m_bStatus = status; }
 	bool		GetStatus( void ) { return m_bStatus; }
@@ -72,7 +113,7 @@ public:
 
 private:
 
-	CUtlString	m_strQuery;
+	std::string	m_strQuery;
 	int			m_iCallback;
 	int			m_iCallbackRef;
 	
@@ -82,11 +123,14 @@ private:
 	MYSQL_RES*	m_myResult;
 	int			m_iLastID;
 
-	CUtlString	m_strError;
+	std::string	m_strError;
 
 #ifdef ENABLE_QUERY_TIMERS
 	Timer		m_queryTimer;
 #endif
+
+public:
+	Query*		next;
 };
 
 class Database
@@ -95,32 +139,54 @@ public:
 	Database( const char* host, const char* user, const char* pass, const char* db, int port );
 	~Database( void );
 
-	bool	Initialize( CUtlString& error );
-	bool	IsSafeToShutdown( void );
+	bool	Initialize( std::string& error );
 	void	Shutdown( void );
+	std::size_t RunShutdownWork( void );
+	void	Release( void );
 
-	bool		SetCharacterSet( const char* charset, CUtlString& error );
+	bool		SetCharacterSet( const char* charset, std::string& error );
 	char*		Escape( const char* query );
 	void		QueueQuery( const char* query, int callback = -1, int flags = 0, int callbackref = -1 );
 
-	CUtlVectorMT<CUtlVector<Query*> >& CompletedQueries( void ) { return m_vecCompleted; }
+	Query*		GetCompletedQueries();
 
 private:
-	bool Connect( MYSQL* mysql, CUtlString& error );
+	bool Connect( MYSQL* mysql, std::string& error );
 
 	void QueueQuery( Query* query );
 
 	void DoExecute( Query* query );
-	void YieldPostCompleted( Query* query );
+	void PushCompleted( Query* query );
 
-	IThreadPool*	m_pThreadPool;
+	MYSQL* GetAvailableConnection() 
+	{
+		std::lock_guard<std::recursive_mutex> guard(m_AvailableMutex);
 
-	CUtlVectorMT<CUtlVector<Query*> >						m_vecCompleted;
-	CUtlVectorMT<CUtlVector<MYSQL*> >						m_vecAvailableConnections;
+		MYSQL* result = m_vecAvailableConnections.front();
+		m_vecAvailableConnections.pop_front();
 
-	CUtlString		m_strHost;
-	CUtlString		m_strUser;
-	CUtlString		m_strPass;
-	CUtlString		m_strDB;
-	uint			m_iPort;
+		return result;
+	}
+
+	void ReturnConnection(MYSQL* mysql)
+	{
+		std::lock_guard<std::recursive_mutex> guard(m_AvailableMutex);
+
+		m_vecAvailableConnections.push_back( mysql );
+	}
+
+	waitfree_query_queue<Query> m_completedQueries;
+	std::deque< MYSQL* > m_vecAvailableConnections;
+
+	mutable std::recursive_mutex m_AvailableMutex;
+
+	std::vector<std::thread> thread_group;
+	asio::io_service io_service;
+	std::auto_ptr<asio::io_service::work> work;
+
+	std::string		m_strHost;
+	std::string		m_strUser;
+	std::string		m_strPass;
+	std::string		m_strDB;
+	int				m_iPort;
 };
